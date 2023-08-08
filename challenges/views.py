560f0bdac0482621892e665aa88a741e5b484a0c
Jordan Schlak <jaschlak@gmail.com>
2023-08-07 20:04:37 -0500

    added lookup dictionary

--- challenges/views.py
+++ challenges/views.py
@@ -5,6 +5,22 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
+monthly_challenges = {
+    "january":"Eat no meat for the entire month!",
+    "february":"Walk at least 20 minutes every day!",
+    "march":"Learn Django for at least 20 minutes every day!",
+    "april":"Do the duggie all night and all day",
+    "may":"Look at your toes like you want to lick them with your fingers",
+    "june":"Eat chicken wings once a week",
+    "july":"Eat frogs once",
+    "august":"Get apple",
+    "september":"Put a rose on a forehead",
+    "october":"rock socks",
+    "november":"Be gone until November",
+    "december":"Juggle on a ball"
+    }
+
+
 '''
 # static view routes
 def january(request):
@@ -14,6 +30,7 @@ def february(request):
     return HttpResponse("Walk at least 20 minutes every day!")
 '''
 
+'''
 
 def monthly_challenge_by_number(request,month):
     return HttpResponse(month)
@@ -33,3 +50,15 @@ def monthly_challenge(request,month):
     
     return HttpResponse(challenge_text)
 
+'''
+
+def monthly_challenge_by_number(request,month):
+    return HttpResponse(month)
+
+# dynamic view route
+def monthly_challenge(request,month):
+    try:
+        challenge_text = monthly_challenges[month.lower()]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
