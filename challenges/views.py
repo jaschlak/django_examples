f774ae1b15c6af00da8708e95537e4dd88f09f72
Jordan Schlak <jaschlak@gmail.com>
2023-08-16 09:47:05 -0500

    added udemies variable name

--- challenges/views.py
+++ challenges/views.py
@@ -54,7 +54,7 @@ def monthly_challenge(request,month):
         challenge_text = monthly_challenges[month.lower()]
         #response_date = render_to_string("challenges/challenge.html")
         return render(request, "challenges/challenge.html", {
-            "month": month.capitalize(),
+            "month_name": month.capitalize(),
             "text": challenge_text,
             })
     except:
