5364b2731c7e02da5c1c6c46855d88efc086a386
Jordan Schlak <jaschlak@gmail.com>
2023-08-16 11:43:54 -0500

    added custom 404 message using 404.html

--- challenges/views.py
+++ challenges/views.py
@@ -53,4 +53,5 @@ def monthly_challenge(request,month):
             "text": challenge_text,
             })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
