3029905935ed1912b3d0c55f8eed25cb50dac1aa
Jordan Schlak <jaschlak@dmp.com>
2023-08-29 07:23:16 -0500

    added slugs to model, still more to do for slug functionality

--- section6_bookstore/book_outlet/models.py
+++ section6_bookstore/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -11,8 +12,18 @@ class Book(models.Model):
     author = models.CharField(null=True, max_length=100) # allows nulls
     is_bestselling = models.BooleanField(default=False)  # default null values to False
     
+    # can use model name in url IE http://localhost:8000/<Book.title value>, fills space with -
+    slug = models.SlugField(default="", null=False) 
+    
     def get_absolute_url(self):
         return reverse("book-detail",args=[self.id])
+    
+    # override built in save method and pass args to built in method
+    def save(self, *args, **kwargs):
+        
+        self.slug = slugify(self.title) # slug the Book model's title
+
+        super().save( *args, **kwargs)  # make sure djangos method still called
     
     def __str__(self):
         return "{} ({})".format(self.title, self.rating)
