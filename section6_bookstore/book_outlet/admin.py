7cf4c1b063a386a61753a1f634119e16ed3fad6b
Jordan Schlak <jaschlak@dmp.com>
2023-09-05 08:00:25 -0500

    admin- list filterable items on admin page

--- section6_bookstore/book_outlet/admin.py
+++ section6_bookstore/book_outlet/admin.py
@@ -8,7 +8,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
-    
+    list_filter =  ("author","rating")
     
 admin.site.register(Book, BookAdmin)
     
