d7239611a87ac3b2554d63dd00ff42e2fcbd4048
Jordan Schlak <jaschlak@dmp.com>
2023-09-12 13:28:02 -0500

    added model with foreign key, cleared db for compatibility

--- section6_bookstore/book_outlet/models.py
+++ section6_bookstore/book_outlet/models.py
@@ -4,31 +4,22 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
 class Book(models.Model):
     # Id created automatically
     title = models.CharField(max_length=50) # From django model fields
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100) # allows nulls
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE, null=True) # null values acceptable
     is_bestselling = models.BooleanField(default=False)  # default null values to False
     
     # can use model name in url IE http://localhost:8000/<Book.title value>, fills space with -
-    slug = models.SlugField(default="", null=False, db_index=True) 
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True) 
     
     def get_absolute_url(self):
         return reverse("book-detail",args=[self.slug])
-    
-    '''
-    note: save method not necessary after making the admin model 
-                                         and pre-populating slug
-    
-    # override built in save method and pass args to built in method
-    def save(self, *args, **kwargs):
-        
-        self.slug = slugify(self.title) # slug the Book model's title
-
-        super().save( *args, **kwargs)  # make sure djangos method still called
-    '''
     
     def __str__(self):
         return "{} ({})".format(self.title, self.rating)
