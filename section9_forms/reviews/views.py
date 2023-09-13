5776eb74ba86df1709fb0018a60279a13ccb06eb
Jordan Schlak <jaschlak@dmp.com>
2023-09-13 12:47:44 -0500

    render POST redirect to thank you, crude render instead of redirect

--- section9_forms/reviews/views.py
+++ section9_forms/reviews/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 def review(request):
     
+    if request.method == "POST":
+        entered_username = request.POST['username']
+        print(entered_username)
+        return render(request, "reviews/thank_you.html")
+        
     return render(request, 'reviews/review.html')
