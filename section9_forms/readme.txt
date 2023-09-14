376d3278ae93e9e22abfce017c98e712996ceb0e
Jordan Schlak <jaschlak@dmp.com>
2023-09-14 10:17:35 -0500

    forms-modified form error messages and validation criteria

---
+++ section9_forms/readme.txt
@@ -0,0 +1,7 @@
+
+# ReadMe
+
+## Docs
+    
+    # Django Form Fields
+    https://docs.djangoproject.com/en/4.2/ref/forms/fields/
--- section9_forms/reviews/forms.py
+++ section9_forms/reviews/forms.py
@@ -3,5 +3,9 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    
+    user_name = forms.CharField(label="Your Name", required=True, max_length=100, error_messages={
+            "required": "Your name must not be empty like your soul",
+            "max_length": "Please enter a shorter name!"
+        })
     
