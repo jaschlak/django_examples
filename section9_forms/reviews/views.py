b5ea95f13c980a19d420f6d01c1a06ca92ee0d8d
Jordan Schlak <jaschlak@dmp.com>
2023-09-14 09:34:13 -0500

    added form validation errors to request response

--- section9_forms/reviews/views.py
+++ section9_forms/reviews/views.py
@@ -8,17 +8,15 @@ from .forms import ReviewForm
 
 
 def review(request):
-    
-    
     if request.method == "POST":
         form = ReviewForm(request.POST)
         
         if form.is_valid():
-        
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
-    
-    form = ReviewForm()    
+        
+    else:
+        form = ReviewForm()    
     
     return render(request, 'reviews/review.html', {
         "form": form
